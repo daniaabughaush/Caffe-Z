463b63616a28f444a17b41fa01eee73cd07ea645
daniaabughaush <abughoush.dania@gmail.com>
2023-03-21 15:27:33 +0300

    Dom

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let customer = prompt("Please tell us  your name");
+let  customer= prompt("Please tell us  your name");
+let  age= prompt("Please tell us  your age");
 
 
 let pronounce;
@@ -43,3 +44,24 @@ for (let index = 0; index < customerArray.length; index++) {
     console.log(element)
 
 }
+
+let customerInfo=document.getElementById("customerInfo");
+console.log(customerInfo);
+let pEl=document.createElement("p");
+customerInfo.appendChild(pEl);
+pEl.textContent=customer
+
+let ulEl=document.createElement("ul");
+    customerInfo.appendChild(ulEl);
+let liEl2=document.createElement("li");
+     ulEl.appendChild(liEl2);
+     liEl2.textContent="Gender: "+genderLower
+
+       
+let liEl1=document.createElement("li");
+     ulEl.appendChild(liEl1);
+     liEl1.textContent="Age: "+age
+     
+let liEl3=document.createElement("li");
+          ulEl.appendChild(liEl3);
+          liEl3.textContent="Drink: "+drink+" "+drinkName
