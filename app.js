bf74e7a06ad7c2325059215cbda7d427d92e5cec
daniaabughaush <abughoush.dania@gmail.com>
2023-03-21 11:36:10 +0300

    delete repeated code

--- app.js
+++ app.js
@@ -6,20 +6,7 @@ let genderLower;
 let gender;
 function askGender(){
 
-    gender = prompt("please enter your gender", "male or female")
     
-     genderLower = gender.toLowerCase();
-    
-    if (genderLower == "male") {
-        alert("Welcome to Coffe time shop Mr" + " " + customer);
-        pronounce = "Mr"
-    }
-    else if (genderLower == "female") {
-        alert("Welcome to Coffe time shop MS" + " " + customer);
-        pronounce = "Ms"
-    
-    }
-    else {
         while (genderLower !== "male" && genderLower !== "female") {
             gender = prompt("please enter your gender", "male or female")
             genderLower = gender.toLowerCase();
@@ -35,8 +22,7 @@ function askGender(){
     
     
         }
-    }
-
+    
 }
 askGender();
 
--- extra.js
+++ extra.js
@@ -1,29 +1,40 @@
-let x=3*-9
-console.log(x)
+// let x=3*-9
+// console.log(x)
 
-let value="valueis" +50
-console.log(value)
+// let value="valueis" +50
+// console.log(value)
 
-let y=17%5
-y=5%17
-y=5/10
-console.log(y)
-if(4==4){
-    console.log('right')
-}else{
-    console.log("false")
+// let y=17%5
+// y=5%17
+// y=5/10
+// console.log(y)
+// if(4==4){
+//     console.log('right')
+// }else{
+//     console.log("false")
+// }
+
+// if(4!=4){
+//     console.log('true')
+// }else{
+//     console.log("false")
+// }
+
+
+
+let arr=[]
+for (let index = 0; index <=5 ; index++) {
+    arr.push(index)
+
+    // console.log(arr)
 }
 
-if(4!=4){
-    console.log('true')
-}else{
-    console.log("false")
-}
+alert(arr)
 
-if(7<=8){
-    console.log('true')
-}else{
-    console.log("false")
-}
+// if(7<=8){
+//     console.log('true')
+// }else{
+//     console.log("false")
+// }
 
 
